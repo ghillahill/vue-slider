52c057c3e644210347519714a0931d9eb37d6285
Alberto <thebertsinfo@gmail.com>
2020-11-09 16:20:20 +0100

    Creato Slider Basic, con tasto prev e next.
    Aggiunte 4 immagini.jpg dentro cartella img.

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 var app = new Vue({
     el: '#root',
     data: {
-
+        current_index: 0,
+        immagini:[
+            'img/city_one.jpg',
+            'img/city_two.jpg',
+            'img/city_three.jpg',
+            'img/city_four.jpg'
+        ]
     },
     methods: {
-
+        next_image(){
+            this.current_index += 1;
+            if (this.current_index == 4) {
+                this.current_index = 0;
+            }
+        },
+        prev_image(){
+            this.current_index -= 1;
+            if (this.current_index < 0) {
+                this.current_index = 3;
+            }
+        }
     }
 });
